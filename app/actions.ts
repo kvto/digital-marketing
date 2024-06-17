5cd358b004bce5fb83ea024d035af7db851d4db1
Kevin J. Monter Zea <kjms.15987@gmail.com>
2024-06-17 16:23:18 -0500

    Uopdate 17-06-2024

---
+++ app/actions.ts
@@ -0,0 +1,55 @@
+"use server"
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import {z} from "zod"
+
+export type State = {
+    status: "error" | "success" | undefined;
+    errors?: {
+        [key: string]: string[];
+    };
+    message?: string | null;
+}
+
+const productSchema = z.object({
+    name: z
+    .string()
+    .min(3, {message: "El nombre necesita tener mas 5 caracteres"}),
+    category: z.string().min(1, {message: "Categoria es requerida"}),
+    price: z.number().min(1, {message: "El precio debe ser mayor a 1"}),
+    smallDescription: z.string().min(10, {message: "Por favor, describa su producto"}),
+    description: z.string().min(10, {message: "Descripcion es requerida"}),
+    images: z.array(z.string(), {message: "Imagenes son requerida"}),
+    productFile: z
+    .string()
+    .min(1, {message: "Por favor, subir el archivo zip "})
+})
+
+export async function SellProduct(prevState: any, formData: FormData){
+    const {getUser} = getKindeServerSession(); 
+    const user = await getUser();
+
+    if(!user){
+        throw new Error("Something went wrong");
+    }
+
+    const validateFields = productSchema.safeParse({
+        name: formData.get("name"),
+        category: formData.get("category"),
+        price: formData.get("price"),
+        smallDescription: formData.get("smallDescription"),
+        description: formData.get("description"),
+        images: formData.get("images"),
+        productFile: formData.get("productFile")
+    });
+
+    if(!validateFields.success){
+        const state: State = {
+            status: "error",
+            errors: validateFields.error.flatten().fieldErrors,
+            message: "Ops, existe un error con alguno de los datos..."
+        };
+
+        return state;
+    }
+}
--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -6,12 +8,23 @@ import { Textarea } from "@/components/ui/textarea";
 import { TipTapEditor } from "../components/Editor";
 import { UploadDropzone } from "../lib/uploadthing";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { JSONContent } from "@tiptap/react";
+import { SellProduct, State } from "../actions";
+import { useFormState } from "react-dom";
 
 export default function SellRoute(){
+    const initalState: State = {message: "", status: undefined};
+    const [state, formAction] = useFormState(SellProduct, initalState);
+    const [json, setJson] = useState<null | JSONContent>(null);
+    const [images, setImages] = useState<null | string[]>(null);
+    const [productFile, SetProductFile] = useState<null | string>(null);
+
+    console.log(state?.message)
     return(
         <section className="max-w-7xl mx-autp px-4 md:px-8 mb-14">
             <Card>
-                <form>
+                <form action={formAction}>
                     <CardHeader>
                         <CardTitle>Vender tu producto de facil manera</CardTitle>
                         <CardDescription>
@@ -21,34 +34,65 @@ export default function SellRoute(){
                     <CardContent className="flex flex-col gap-y-10">
                         <div className="flex flex-col gap-y-2">
                             <Label>Nombre</Label>
-                            <Input type="text" placeholder="Nombre del producto"/>
+                            <Input name="name" type="text" placeholder="Nombre del producto"/>
+                            {state?.errors?.["name"]?.[0] && (
+                                <p className="text-destructive">{state?.errors?.["name"]?.[0]}</p>
+                            )}
                         </div>
                         <div className="flex flex-col gap-y-2">
                             <Label>Categoria</Label>
                             <SelectCategory />
+                            {state?.errors?.["category"]?.[0] && (
+                                <p className="text-destructive">{state?.errors?.["category"]?.[0]}</p>
+                            )}
                         </div>
                         <div className="flex flex-col gap-y-2">
                             <Label>Precio</Label>
-                            <Input placeholder="29$" type="number"/>
+                            <Input placeholder="29$" type="number" name="price"/>
+                            {state?.errors?.["price"]?.[0] && (
+                                <p className="text-destructive">{state?.errors?.["price"]?.[0]}</p>
+                            )}
                         </div>
 
                         <div className="flex flex-col gap-y-2">
                             <Label>Sumario</Label>
-                            <Textarea placeholder="Por favor, coloca una descripcion del producto aca..."/>
+                            <Textarea name="smallDescription" placeholder="Por favor, coloca una descripcion del producto aca..."/>
+                            {state?.errors?.["smallDescription"]?.[0] && (
+                                <p className="text-destructive">{state?.errors?.["smallDescription"]?.[0]}</p>
+                            )}
                         </div>
                         <div className="flex flex-col gay-y-2">
+                            <input type="hidden" name="description" value={JSON.stringify(json)}/>
                             <Label>Descripcion</Label>
-                            <TipTapEditor />
+                            <TipTapEditor json={json} setJson={setJson}/>
+                            {state?.errors?.["description"]?.[0] && (
+                                <p className="text-destructive">{state?.errors?.["description"]?.[0]}</p>
+                            )}
                         </div>
 
                         <div className="flex flex-col gap-y-2">
+                            <input type="hidden" name="images" value={JSON.stringify(images)} />
                             <Label>Imagen del producto</Label>
-                            <UploadDropzone endpoint="imageUploader"/>
+                            <UploadDropzone endpoint="imageUploader"
+                            onClientUploadComplete={(res) =>{
+                                setImages(res.map((item) => item.url))
+                            }}
+                            onUploadError={(error: Error) => {
+                                throw new Error(`${error}`)
+                            }}/>
                         </div>
 
                         <div className="flex flex-col gap-y-2">
+                            <input type="hidden" name="productFile" value={productFile ?? " "} />
                             <Label>Archivo del producto</Label>
-                            <UploadDropzone endpoint="productFileUpload"/>
+                            <UploadDropzone 
+                            onClientUploadComplete={(res) => {
+                                SetProductFile(res[0].url);
+                            }}
+                            endpoint="productFileUpload"
+                            onUploadError={(error: Error) => {
+                                throw new Error(`${error}`)
+                            }}/>
                         </div>
                     </CardContent>
                     <CardFooter>
